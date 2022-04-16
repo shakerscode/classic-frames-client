1418457fbfc6d0df0ab20295d8ac590bad232265
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-04-17 05:10:53 +0600

    respomsive footer done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Home from './Home/Home';
+import Footer from './Shared/Footer/Footer';
 import Navbar from './Shared/Navbar/Navbar';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Navbar></Navbar>
         <Home></Home>
+        <Footer></Footer>
     </div>
   );
 }
--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,11 +1,31 @@
 import React from 'react';
+
+import facebook from '../../images/Icons/facebook.png'
+import instagram from '../../images/Icons/instagram.png'
+import linkdin from '../../images/Icons/linkedin.png'
+import twitter from '../../images/Icons/twitter-sign.png'
 import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className='footer-section'>
+            <h1 className='footer-logo'>Classic Frames</h1>
+           <div className='footer-pages'>
+           <p>Privacy and Police</p>
+            <p>Terms and Conditions</p>
+            <p>Help</p>
+            <p>FAQ</p>
+           </div>
+            <div className='icon-sec'>
+                <img src={facebook} alt="" />
+                <img src={instagram} alt="" />
+                <img src={linkdin} alt="" />
+                <img src={twitter} alt="" />
+            </div>
+            <div className='footer-credit'>
+                <p>Copyright &copy; {new Date().getFullYear()} All right reserved by Shaker Ahamed</p>
+            </div>
+        </footer> 
     );
 };
 
