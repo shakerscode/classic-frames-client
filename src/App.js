6015abf9843634837dd74d2e07b5195cd6003390
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-04-17 05:26:04 +0600

    responsive navbar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Home from './Home/Home';
+import Navbar from './Shared/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-       
+    <div>
+      <Navbar></Navbar>
+        <Home></Home>
     </div>
   );
 }
---
+++ src/Shared/Navbar/Navbar.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
+
+import './Navbar.css'
+
+const Navbar = () => {
+    const [toggleIcon, setToggleIcon] = useState(false);
+     
+
+    return (
+       <div className='home-banner '>
+           <div className='header-bg'>
+           <nav className='nav-bar'>
+             <h1 className='logo-section'>Classic Frames</h1>
+             <ul className={toggleIcon ? 'nav-link-mobile ' : 'nav-link'} onClick={()=>setToggleIcon(!toggleIcon)}>
+                    <Link className='links' to='/'>Home</Link>
+                    <Link className='links' to='/'>Services</Link>
+                    <Link className='links' to='/'>About Me</Link>
+                    <Link className='links' to='/'>Contact Me</Link>
+                    <Link className='links' to='/'>Login</Link>
+             </ul>
+
+            <div className='btn menu-icon' onClick={()=>setToggleIcon(!toggleIcon)}>
+            {
+                 toggleIcon ? 
+                 <XIcon className=''></XIcon>
+
+                 : 
+                 <MenuIcon className=''></MenuIcon>
+             }
+            </div>
+
+         </nav>
+           </div>
+       </div>
+    );
+};
+
+export default Navbar;
