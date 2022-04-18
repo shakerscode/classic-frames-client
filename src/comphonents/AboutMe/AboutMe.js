aa898631b1b0cc4645f24728ac7aa908fca7ddab
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-04-18 20:27:01 +0600

    blog done

--- src/comphonents/AboutMe/AboutMe.js
+++ src/comphonents/AboutMe/AboutMe.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './AboutMe.css'
+import myPic from '../../images/me.png'
 
 const AboutMe = () => {
     return (
         <div>
-            <h1>About me</h1>
+            <div className="about-me">
+                <div className='my-image'>
+                    <img src={myPic} alt="" />
+                </div>
+                <div className='my-info'>
+                    <p>
+                        
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/comphonents/Blog/Blog.js
+++ src/comphonents/Blog/Blog.js
@@ -8,14 +8,31 @@ const Blog = () => {
                 <div className="post">
                     <h4> Difference between authorization and authentication</h4>
                     <hr />
+                    <p>
+                        By authentication, we can verify a user. It actually contain users identity. It's a process of identifying a user and validating the identity of a user.
+                        Authorization is a process that happen when the authentication is successful.With this process, we can define what resource a user can access. It gives permission to a user to access some specific or spatial data or anything else.
+                    </p>
                 </div>
                 <div className="post">
-                <h4> Why are you using firebase? What other options do you have to implement authentication?</h4>
-                <hr />
+                    <h4> Why are you using firebase? What other options do you have to implement authentication?</h4>
+                    <hr />
+                    <p>It's an easier process to get connected with users.It provide simple backend services, ready made UI libraries etc. Its authentication system is easy to build and pretty much maintained. It support Google, Facebook, GitHub, Twitter, Email/Password, Email/Password, Phone, Phone, Apple, Anonymous, Microsoft, Twitter and Yahoo authentication system.
+                        <br />
+                        <b>The other option to implement authentication is: </b>
+                        <br />
+                        Single-Factor/Primary Authentication,
+                        Two-Factor Authentication (2FA),
+                        Single Sign-On (SSO),
+                        Multi-Factor Authentication (MFA),
+                        Password Authentication Protocol (PAP),
+                        Challenge Handshake Authentication Protocol (CHAP) and
+                        Extensible Authentication Protocol (EAP)
+                    </p>
                 </div>
                 <div className="post">
-               <h4>What other services does firebase provide other than authentication?</h4>
-               <hr />
+                    <h4>What other services does firebase provide other than authentication?</h4>
+                    <hr />
+                    <p>It provides backend services, easy to use SDKs and ready-made ui libraries to authenticate with users. It provide several authentication system that i mentioned earlier.Beside this, it also provide hosting, google analytics, cloud storage, cloud function, cloud massiging  and many more. And it almost free for a short services website.</p>
                 </div>
             </div>
         </div>
